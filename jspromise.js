092caad5a3ba7833139e9f2b82c7600f6532102a
MikaelAbyss <65512888+MikaelAbyss@users.noreply.github.com>
2021-05-07 22:15:37 -0300

    Add files via upload

---
+++ jspromise.js
@@ -0,0 +1,26 @@
+let p = new Promise((resolve, reject) => {
+    const a = {
+        name: 'Mikael',
+        message: Mikael,
+    }
+    if (message = 'Mikael') {
+        resolve('Este é o cara!')
+    } else {
+        reject('ele não e brabo')
+    }
+})
+.then((message) => {
+    console.log('Olha so: ')
+}).catch((err) => {
+    console.log('Q pena')
+})
+
+function watchTheTime () {
+    const a = {
+        name: 'Lucas',
+        message: 22,
+    }
+    console.log(a);
+}
+
+watchTheTime ();
